fb81bdf43c25b69e61a7a2809e7ce7bf97d3b228
Michael Carroll <michael@carroll.io>
2014-07-09 07:21:23 -0400

    Update e-mail and URL constants for staging server

--- config/initializers/_site_constants.rb
+++ config/initializers/_site_constants.rb
@@ -1,5 +1,5 @@
-EMAIL_URI           = 'lets.gopushcart.com'
+EMAIL_URI           = Rails.env.staging? ? 'sandbox.gopushcart.com' : 'lets.gopushcart.com'
 SITE_NAME           = 'pushcart'
-SITE_URL            = 'gopushcart.com'
+SITE_URL            = Rails.env.staging? ? 'sandbox.gopushcart.com' : 'gopushcart.com'
 SECRET_KEY          = Rails.application.secrets.aes_secret_key
 USE_PRIVACY_HASHING = true
