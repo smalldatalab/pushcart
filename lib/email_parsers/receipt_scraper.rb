121bf84b01ec68e7bc71172ed3b61a1d55ec256f
Michael Carroll <michael@carroll.io>
2014-08-16 22:31:26 -0400

    Improvements to FD scraper

--- lib/email_parsers/receipt_scraper.rb
+++ lib/email_parsers/receipt_scraper.rb
@@ -14,8 +14,19 @@ private
     if element.nil?
       return false
     else
-      return element.gsub(/(\s|px)/, '') == characteristic_string.gsub(/(\s|px)/, '')
+      return clean_string(element) == clean_string(characteristic_string)
     end
+  end
+
+  def clean_string(el)
+    # Remove spaces & px
+    clean = el.gsub(/(\s|px)/, '')
+    # Downcase
+    clean = clean.downcase
+    #Chop off trailing ;
+    clean = clean.gsub(/;$/, '')
+
+    return clean
   end
 
 end
--- spec/factories/emails.rb
+++ spec/factories/emails.rb
@@ -27,6 +27,13 @@ FactoryGirl.define do
       raw_text File.read(Rails.root.to_s + '/lib/sample_emails/fresh_direct/receipt_three.eml')
     end
 
+    trait :fresh_direct_receipt_four do
+      from 'receipt@freshdirect.com'
+      subject 'Fwd: Your order for Tuesday, Aug 5 2014'
+      raw_html File.read(Rails.root.to_s + '/lib/sample_emails/fresh_direct/receipt_four.eml')
+      raw_text File.read(Rails.root.to_s + '/lib/sample_emails/fresh_direct/receipt_four.eml')
+    end
+
     trait :instacart_receipt do
       from 'orders@instacart.com'
       subject 'Fwd: Your Order with Instacart'
