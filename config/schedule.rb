da4d7f2c67179c7e003c0d9b2f0e1d97ae83253a
Michael Carroll <michael@carroll.io>
2014-10-09 11:54:20 -0400

    Fix cron task typo; update weekly digest image assets to use S3

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,5 @@ set :output, "#{path}/log/cron.log"
 set :environment, ENV['RAILS_ENV']
 
 every :monday, at: '9:00 am' do
-  rake 'weekly_digest:mail_all_users'
+  rake 'weekly_digester:mail_all_users'
 end
