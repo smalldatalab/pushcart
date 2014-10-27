0460d24f76d89d34f3e6a48adecaf1c3b0c8888f
Michael Carroll <michael@carroll.io>
2014-10-27 13:35:03 -0400

    Temporarily shut down weekly mailers

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, "#{path}/log/cron.log"
 
 set :environment, ENV['RAILS_ENV']
 
-every :monday, at: '9:00 am' do
-  rake 'weekly_digester:mail_all_users'
-end
+# every :monday, at: '9:00 am' do
+#   rake 'weekly_digester:mail_all_users'
+# end
