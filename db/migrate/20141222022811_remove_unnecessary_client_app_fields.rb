c665c3200fa62ebd0d1dd3e4a843fa476b286ec0
Michael Carroll <michael@carroll.io>
2014-12-21 22:12:03 -0500

    Update migration to remove redundancy

--- db/migrate/20141222022811_remove_unnecessary_client_app_fields.rb
+++ db/migrate/20141222022811_remove_unnecessary_client_app_fields.rb
@@ -1,6 +1,5 @@
 class RemoveUnnecessaryClientAppFields < ActiveRecord::Migration
   def change
-    remove_reference :messages, :oauth_application
     remove_column :oauth_application, :endpoint_email, :string
   end
 end
