1d8cb7213dd816c5a0b1c58fbab2375454613ffd
Michael Carroll <michael@carroll.io>
2014-12-21 22:15:39 -0500

    Did it again...

--- db/migrate/20141222022811_remove_unnecessary_client_app_fields.rb
+++ db/migrate/20141222022811_remove_unnecessary_client_app_fields.rb
@@ -1,5 +1,5 @@
 class RemoveUnnecessaryClientAppFields < ActiveRecord::Migration
   def change
-    remove_reference :messages, :oauth_applications
+    remove_column :oauth_application, :endpoint_email, :string
   end
 end
