01de1d2abbea0a4e44f7ad774848645a7808dea1
Michael Carroll <michael@carroll.io>
2016-02-23 11:53:54 -0500

    Fix this migration, too

--- db/migrate/20150325080719_update_swaps.rb
+++ db/migrate/20150325080719_update_swaps.rb
@@ -1,6 +1,6 @@
 class UpdateSwaps < ActiveRecord::Migration
   def change
-    add_column :itemizables, :swap_id,  :integer, index: true
+    # add_column :itemizables, :swap_id,  :integer, index: true
     add_column :itemizables, :coach_id, :integer, index: true
 
     drop_table :swap_suggestions
